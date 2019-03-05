6ed774add161fb1ee98b0cb8d5995da41dc42d41
Christian Pérez Villanueva <christian.perez34@upr.edu>
2019-03-05 16:22:40 -0400

    Added resize method for gif's

--- animations/gif.py
+++ animations/gif.py
@@ -1,4 +1,5 @@
-from moviepy.video.io.VideoFileClip import VideoFileClip
+# from moviepy.video.io.VideoFileClip import VideoFileClip
+import moviepy.editor as mp
 
 
 class Gif:
@@ -6,12 +7,20 @@ class Gif:
     def __init__(self):
         self.clip = None
 
-    def create_gif(self, clip, start_time, end_time, resize=None):
+    def make_gif(self, clip, start_time, end_time):
         """
         Slices a video file between two time frames to create a gif
         :param clip: Name of video file
         :param start_time: Tuple stating time in (hour, min, sec), (min, sec) or (sec)
         :param end_time: Tuple stating time in (hour, min, sec), (min, sec) or (sec)
-        :param resize: Resize's video
         """
-        self.clip = VideoFileClip(clip).subclip(start_time, end_time).resize(resize)
+        self.clip = mp.VideoFileClip(clip).subclip(start_time, end_time)
+
+    def resize(self, new_size):
+        """
+        Uses moviepy.video.fx.all.resize module
+        :param new_size: Can be wither(width,height) in pixels or a float
+                         A scaling factor, like 0.5
+                         A function of time returning one of these.
+        """
+        self.clip = self.clip.resize(new_size)
--- main.py
+++ main.py
@@ -6,9 +6,10 @@ from animations.gif import Gif
 def main():
     clip = os.getenv('USERPROFILE') + '\\Downloads\\CHOLO Voice Trolling on FORTNITE ft. Lil Moco!.mp4'
     gif = Gif()
-    gif.create_gif(clip=clip, start_time=(0, 24), end_time=(0, 27), resize=0.3)
-    gif.clip = gif.clip.crop(x1=60, x2=300)
-    gif.clip.write_gif('test8.gif')
+    gif.make_gif(clip=clip, start_time=(0, 24), end_time=(0, 27))
+    gif.resize(new_size=0.6)
+    # gif.clip = gif.clip.crop(x1=60, x2=300)
+    gif.clip.write_gif('test1.gif')
 
 
 if __name__ == '__main__':
