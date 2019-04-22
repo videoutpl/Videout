3ad8098b4c28f0261ef952aca0b3531a2f9be888
Lexdyel Mendez <lexdyel.mendez@upr.edu>
2019-04-22 17:58:25 -0400

    Fixed merge. Deleted previews merge characters.

--- src/loader.py
+++ src/loader.py
@@ -1,10 +1,6 @@
 import lexer
 
-
 while True:
-<<<<<<< HEAD
-    text = input('-->')
-=======
     text = input('--> ')
 >>>>>>> 2594524b64dbdb57d567e21d4d099ea170c43dcc
     result , error = lexer.run('<stdin>',text)
